704fe6ed3ff6e4ddba11637d9d3a72b033a537f6
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-01 08:33:27 +0300

    commit tree 28f822afbff96ecf1075022e32a2f8159ded1ca0

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,17 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] };
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case "friends/add":
+      return {
+        ...state,
+        friends: [...state.friends,action.payload ],
+      };
+    case "friends/remove":
+      return{
+        ...state,
+        friends:state.friends.filter(friend=>friend.id!==action.payload),
+      }
+    default:
+      return state;
+  }
 }
